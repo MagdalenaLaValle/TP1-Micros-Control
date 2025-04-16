509a58e9aa2f72075ef686d713eca9eabd7337c9
Juangonz952 <jgonzalezmartin@itba.edu.ar>
2025-04-16 14:06:46 -0300

    ejercicio 6 coqueto, faltan terminaciones meramente esteticas

--- Ej6/vectores.c
+++ Ej6/vectores.c
@@ -3,6 +3,16 @@
 /* librerias */
 #include "vectores.h"
 
+/* prototipos*/
+void imprimir_vector(vector_t *v);
+double norma_2(vector_t *vec);
+vector_t Suma(vector_t *vec1, vector_t *vec2);
+float Producto_Interno(vector_t *v1, vector_t *v2);
+vector_t Producto_Externo(vector_t *v1,vector_t *v2);
+vector_t Escalar(vector_t *v,float lambda);
+vector_t Resta(vector_t *v1, vector_t *v2);
+
+
 vector_t inicializar_vector(float x, float y, float z){
     vector_t vec;
     vec.x = x;
@@ -10,9 +20,11 @@ vector_t inicializar_vector(float x, float y, float z){
     vec.z = z;
     vec.norma = norma_2;
     vec.imprimir = imprimir_vector;
-    vec.Adicion = Adicion;
+    vec.Adicion = Suma;
     vec.producto_interno = Producto_Interno;
     vec.producto_X = Producto_Externo;
+    vec.Escalar = Escalar;
+    vec.Resta = Resta;
     return vec;    
 }
 
@@ -21,18 +33,30 @@ double norma_2(vector_t *vec){
 }
 
 void imprimir_vector(vector_t *v){ 
-    printf(" ( %d, %d, %d)", v->x,v->y,v->z);
+    printf(" ( %.2f, %.2f, %.2f)", v->x,v->y,v->z);
 }
 /* funciones */
 
 
 
-vector_t Adicion(vector_t *vec1, int suma_o_resta, vector_t *vec2){
+vector_t Suma(vector_t *vec1, vector_t *vec2){
     vector_t vec_resultado;
-    vec_resultado.x = vec1->x + (vec2->x)*(suma_o_resta);
-    vec_resultado.y = vec1->y + (vec2->y)*(suma_o_resta);
-    vec_resultado.z = vec1->z + (vec2->z)*(suma_o_resta);
+    float x,y,z;
+    x = vec1->x + (vec2->x);
+    y = vec1->y + (vec2->y);
+    z = vec1->z + (vec2->z);
+    vec_resultado = inicializar_vector(x,y,z);
     return vec_resultado;
+}
+
+vector_t Escalar(vector_t *v, float k){
+    vector_t vec = inicializar_vector(k*(v->x),k*(v->y),k*(v->z));
+    return vec;
+}
+
+vector_t Resta(vector_t *v1, vector_t *v2){
+    vector_t aux = Escalar(v2,-1);
+    return Suma(v1,&aux);
 }
 
 float Producto_Interno(vector_t *v1, vector_t *v2){
@@ -41,9 +65,11 @@ float Producto_Interno(vector_t *v1, vector_t *v2){
 
 vector_t Producto_Externo(vector_t *v1,vector_t *v2){
     vector_t vec_resultante;
-    vec_resultante.x = (v1->y)*(v2->z) - (v1->z)*(v2->y);
-    vec_resultante.y = (v1->x)*(v2->z) - (v1->z)*(v2->x);
-    vec_resultante.z = (v1->x)*(v2->y) - (v2->x)*(v1->y);
+    float x,y,z;
+    x = (v1->y)*(v2->z) - (v1->z)*(v2->y);
+    y = (v1->x)*(v2->z) - (v1->z)*(v2->x);
+    z = (v1->x)*(v2->y) - (v2->x)*(v1->y);
+    vec_resultante = inicializar_vector(x,y,z);
     return vec_resultante;
 }
 
