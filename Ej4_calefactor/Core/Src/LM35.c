dd9e6eac212238a73c04a8c91f428024c5b85c15
Juan Bautista Gonzalez Martin <jgonzalezmartin@itba.edu.ar>
2025-06-07 19:05:13 -0300

    Delete Ej4_calefactor directory

--- Ej4_calefactor/Core/Src/LM35.c
+++
@@ -1,9 +0,0 @@
-#include "LM35.h"
-
-float calcTemp_LM35(float voltage){
-	float temp;
-	temp = voltage * PENDIENTE;
-	return temp;
-}
-
-
--- Ej4_calefactor/Debug/Core/Src/LM35.d
+++
@@ -1,2 +0,0 @@
-Core/Src/LM35.o: ../Core/Src/LM35.c ../Core/Inc/LM35.h
-../Core/Inc/LM35.h:
